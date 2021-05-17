ca9f3e5cc815e0ccaa647427d4a1efbdf0e0dc8c
Martin Bernal <mbs9858@springernature.com>
2021-05-17 09:40:24 +0200

    startApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
+// ANGULAR:
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// NGRX:
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todos/todo.reducer';
+
+// MODULES
+import { TodoModule } from './todos/todo.module';
+
+// COMPONETS:
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    TodoModule,
+    StoreModule.forRoot({ todos: todoReducer })]
+  ,
   providers: [],
   bootstrap: [AppComponent]
 })
